576e654de52593dbfbd9cd5d7386e811613b4682
Kate <kate.burovova@gmail.com>
2024-05-19 14:04:29 +0300

    add delimiters

--- app.py
+++ app.py
@@ -11,9 +11,13 @@ st.markdown(r'''
 Betweenness centrality quantifies the number of times a node acts as a bridge along the shortest path between two other nodes.
 ''')
 
+st.markdown('<hr style="border:2px solid gray">', unsafe_allow_html=True)
+
 show_html_graph('assets/recovery_win_betweenness_centrality_size_viz.html')
 
 display_csv_preview(file_path='assets/facebook_sample.csv', file_name='Facebook sample')
+
+st.markdown('<hr style="border:2px solid gray">', unsafe_allow_html=True)
 
 show_download_button(file_path='assets/facebook_sample.csv',
                      label='Download FB sample',
@@ -22,10 +26,14 @@ show_download_button(file_path='assets/facebook_sample.csv',
 
 display_csv_preview(file_path='assets/twitter_sample.csv', file_name='X sample')
 
+st.markdown('<hr style="border:2px solid gray">', unsafe_allow_html=True)
+
 show_download_button(file_path='assets/twitter_sample.csv',
                      label='Download X sample',
                      file_name='X_sample_data.csv',
                      help='Click to download X sample data')
+
+st.markdown('<hr style="border:2px solid gray">', unsafe_allow_html=True)
 
 display_csv_preview(file_path='assets/telegram_sample.csv', file_name='Telegram sample')
 

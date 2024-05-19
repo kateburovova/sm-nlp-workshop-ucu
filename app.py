1d32a5c900fc193cdb7e1943c8928ceb766072d3
Kate <kate.burovova@gmail.com>
2024-05-19 14:11:51 +0300

    change order

--- app.py
+++ app.py
@@ -6,15 +6,6 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
-
-st.markdown(r'''
-Betweenness centrality quantifies the number of times a node acts as a bridge along the shortest path between two other nodes.
-''')
-
-show_html_graph('assets/recovery_win_betweenness_centrality_size_viz.html')
-
-st.markdown('<hr style="border:2px solid gray">', unsafe_allow_html=True)
-
 display_csv_preview(file_path='assets/facebook_sample.csv', file_name='Facebook sample')
 
 show_download_button(file_path='assets/facebook_sample.csv',
@@ -39,4 +30,14 @@ show_download_button(file_path='assets/telegram_sample.csv',
                      label='Download TG sample',
                      file_name='TG_sample_data.csv',
                      help='Click to download TG sample data')
+
+st.markdown('<hr style="border:2px solid gray">', unsafe_allow_html=True)
+
+st.markdown(r'''
+Betweenness centrality quantifies the number of times a node acts as a bridge along the shortest path between two other nodes.
+''')
+
+show_html_graph('assets/recovery_win_betweenness_centrality_size_viz.html')
+
+st.markdown('<hr style="border:2px solid gray">', unsafe_allow_html=True)
 

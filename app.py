64164bbe1b19b672afe9921d26cb2cf8e1e411d3
Kate <kate.burovova@gmail.com>
2024-05-19 12:12:54 +0300

    fix formula

--- app.py
+++ app.py
@@ -37,6 +37,8 @@ st.latex(r'''
 Betweenness centrality quantifies the number of times a node acts as a bridge along the shortest path between two other nodes. Let \\(\\sigma(V_j, V_k)\\) be the number of shortest paths from node \\(V_j\\) to node \\(V_k\\), and \\(\\sigma(V_j, V_k | V_i)\\) the number of those paths that pass through node \\(V_i\\). The betweenness centrality of a node \\(V_i\\) is given by:
 
 C_B(V_i) = \sum_{{V_i \neq V_j \neq V_k \in V}} \frac{\sigma(V_j, V_k | V_i)}{\sigma(V_j, V_k)} \times \frac{2}{(|V| - 1)(|V| - 2)}
+
+where \\(|V|\\) is the number of nodes in the graph.
 ''')
 
 # Additional explanatory text

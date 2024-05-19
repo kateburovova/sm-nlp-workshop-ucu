2f356837c107254d419b46e69e12b9f077f76d31
Kate <kate.burovova@gmail.com>
2024-05-19 12:09:37 +0300

    fix formula

--- app.py
+++ app.py
@@ -31,19 +31,24 @@ import streamlit.components.v1 as components
 
 st.set_page_config(layout="wide")
 
-import streamlit as st
 
-text_and_formula = """
-Betweenness centrality quantifies the number of times a node acts as a bridge along the shortest path between two other nodes. Let \\( \sigma(V_j, V_k) \\) be the number of shortest paths from node \\( V_j \\) to node \\( V_k \\), and \\( \sigma(V_j, V_k \mid V_i) \\) the number of those paths that pass through node \\( V_i \\). The betweenness centrality of a node \\( V_i \\) is given by:
-
-\\[
-C_B(V_i) = \sum_{V_i \neq V_j \neq V_k \in V} \dfrac{\sigma(V_j, V_k \mid V_i)}{\sigma(V_j, V_k)} \times \dfrac{2}{(|V| - 1)(|V| - 2)}
-\\]
-
-where \\( |V| \\) is the number of nodes in the graph.
+mathjax_script = """
+<script src='https://polyfill.io/v3/polyfill.min.js?features=es6'></script>
+<script id='MathJax-script' async src='https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js'></script>
 """
 
-st.markdown(text_and_formula)
+html_content = f"""
+{mathjax_script}
+<div>
+    <p>Betweenness centrality quantifies the number of times a node acts as a bridge along the shortest path between two other nodes. Let \( \sigma(V_j, V_k) \) be the number of shortest paths from node \( V_j \) to node \( V_k \), and \( \sigma(V_j, V_k \mid V_i) \) the number of those paths that pass through node \( V_i \). The betweenness centrality of a node \( V_i \) is given by:</p>
+    <p>\[
+    C_B(V_i) = \sum_{{V_i \neq V_j \neq V_k \in V}} \frac{{\sigma(V_j, V_k \mid V_i)}}{{\sigma(V_j, V_k)}} \times \frac{{2}}{{(|V| - 1)(|V| - 2)}}
+    \]</p>
+    <p>where \( |V| \) is the number of nodes in the graph.</p>
+</div>
+"""
+
+components.html(html_content, height=300)
 
 
 show_html_graph('assets/recovery_win_betweenness_centrality_size_viz.html')

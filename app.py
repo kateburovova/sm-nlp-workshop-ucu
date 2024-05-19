f8d8d3b72e177bd8e2c44ee4e0611fc26b00cbce
Kate <kate.burovova@gmail.com>
2024-05-19 13:46:03 +0300

    refactor download

--- app.py
+++ app.py
@@ -1,26 +1,15 @@
+# Base
 import os
-import streamlit as st
 import time
-from authenticate import check_password
-from utils import show_html_graph
-import streamlit.components.v1 as components
 
-#
-# import plotly.express as px
-# import streamlit.components.v1 as components
-#
-# from langchain import hub
-# from datetime import datetime
-# from langchain import callbacks
-# from elasticsearch import Elasticsearch
-# from elasticsearch import BadRequestError
-# from elasticsearch.exceptions import NotFoundError
-# from angle_emb import AnglE, Prompts
-# from langchain_openai import ChatOpenAI
-# from authentificate import check_password
-# from utils import (display_distribution_charts,populate_default_values, project_indexes,
-#                    populate_terms,create_must_term, create_dataframe_from_response,flat_index_list,
-#                    search_elastic_below_threshold)
+# Internal
+from utils import show_html_graph, show_download_button
+from authenticate import check_password
+
+
+# External
+import streamlit as st
+import streamlit.components.v1 as components
 
 #
 # es_config = {
@@ -38,17 +27,10 @@ Betweenness centrality quantifies the number of times a node acts as a bridge al
 
 show_html_graph('assets/recovery_win_betweenness_centrality_size_viz.html')
 
-file_path = 'assets/facebook_sample.csv'
-
-with open(file_path, "rb") as file:
-    btn = st.download_button(
-        label="Download CSV",
-        data=file,
-        file_name='sample_data.csv',
-        mime='text/csv',
-        help="Click to download the CSV file."
-    )
-
+show_download_button(file_path='assets/facebook_sample.csv',
+                     label='Download FB sample',
+                     file_name='FB_sample_data.csv',
+                     help='Click to download FB sample data')
 
 if not check_password():
     st.stop()
--- utils.py
+++ utils.py
@@ -3,8 +3,18 @@ import streamlit.components.v1 as components
 
 
 def show_html_graph(file_path, height=600):
-
     with open(file_path, 'r', encoding='utf-8') as f:
         html_content = f.read()
 
     components.html(html_content, height=height, scrolling=True)
+
+
+def show_download_button(file_path, label, file_name, help):
+    with open(file_path, "rb") as file:
+        btn = st.download_button(
+            label=label,
+            data=file,
+            file_name=file_name,
+            mime='text/csv',
+            help=help
+        )
